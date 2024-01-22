2e95ffa49943b5ac01560664dd6f4d19c3aaa2fc
Haidaradlan12 <adlannufahaidar12@gmail.com>
2024-01-22 16:47:32 +0700

    tr

---
+++ Assets/Asset Script/ButtonQwerty.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ButtonQwerty : MonoBehaviour
+{
+    public GameObject Nyawa1, Nyawa2, Nyawa3;
+    private int benarnya, salahnya;
+    public int quizindex;
+    public RectTransform PopUpNext, PopUpGame;
+    public AudioSource audiobenar, audiosalah;
+    // Update is called once per frame
+    void Start()
+    {
+        PopUpGame.DOAnchorPos(new Vector2(-1500, 0), 1.0f);
+        PopUpNext.DOAnchorPos(new Vector2(-1500, 0), 1.0f);
+    }
+    void Update()
+    {
+        if (salahnya > 2)
+        {
+            PopUpGame.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        }
+        else if (salahnya == 1)
+        {
+            Nyawa1.SetActive(false);
+            Nyawa2.SetActive(true);
+            Nyawa3.SetActive(true);
+            PopUpGame.DOAnchorPos(new Vector2(-1500, 0), 1.0f);
+        }
+        else if (salahnya == 2)
+        {
+            Nyawa1.SetActive(false);
+            Nyawa2.SetActive(false);
+            Nyawa3.SetActive(true);
+            PopUpGame.DOAnchorPos(new Vector2(-1500, 0), 1.0f);
+            audiosalah.Play();
+        }
+        else
+        {
+            Nyawa1.SetActive(true);
+            Nyawa2.SetActive(true);
+            Nyawa3.SetActive(true);
+            PopUpGame.DOAnchorPos(new Vector2(-1500, 0), 1.0f);
+        }
+
+        if (benarnya == quizindex)
+        {
+            PopUpNext.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        }
+        else if ((benarnya+1) == quizindex)
+        {
+            audiobenar.Play();
+            PopUpNext.DOAnchorPos(new Vector2(-1500, 0), 1.0f);
+        }
+        else
+        {
+            PopUpNext.DOAnchorPos(new Vector2(-1500, 0), 1.0f);
+        }
+    }
+    public void Salah()
+    {
+        salahnya = salahnya + 1;
+    }
+    public void Benar()
+    {
+        benarnya = benarnya + 1;
+    }
+}
---
+++ Assets/Asset Script/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform MenuBurunghantu, MenuButton, MenuInformasi, MenuGuide;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuBurunghantu.DOAnchorPos(new Vector2(0,0), 1.0f);
+        MenuButton.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(-1500, 0), 1.0f);
+        MenuGuide.DOAnchorPos(new Vector2(-1500, 0), 1.0f);
+    }
+    public void MenuKembali()
+    {
+        MenuBurunghantu.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuButton.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(-1500, 0), 1.0f);
+        MenuGuide.DOAnchorPos(new Vector2(-1500, 0), 1.0f);
+    }
+    public void MenuInformasibutton()
+    {
+        MenuBurunghantu.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuButton.DOAnchorPos(new Vector2(0, -1500), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuGuide.DOAnchorPos(new Vector2(-1500, 0), 1.0f);
+    }
+    public void MenuGuidebutton()
+    {
+        MenuBurunghantu.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuButton.DOAnchorPos(new Vector2(0, -1500), 1.0f);
+        MenuInformasi.DOAnchorPos(new Vector2(-1500, 0), 1.0f);
+        MenuGuide.DOAnchorPos(new Vector2(0, 0), 1.0f);
+    }
+
+}
